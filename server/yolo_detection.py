aefc0cb26d7e21dd83c4aa2a8c6b962280f99198
Beh Chun Siang <chunsiang1109@gmail.com>
2024-07-26 16:33:10 +0800

    add counting function for displaying on table

--- server/yolo_detection.py
+++ server/yolo_detection.py
@@ -14,25 +14,14 @@ import torch
 import base64
 
 
+#to control the detection of the camera
+detection_running= False
+
 # Start webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-# Model
-model = YOLO("yolov8n.pt")
-
-# Object classes
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"]
 
 # FPS calculation variables
 start_time = time.time()
@@ -86,7 +75,6 @@ def video_stream():
   
 
     camera1 = pylon.InstantCamera(tl_factory.CreateDevice(devices[0]))
-
     camera1.Open()
     camera1.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
 
@@ -104,80 +92,69 @@ def video_stream():
     frame_count = 0
     prev_time = time.time()
 
+    # Table for results
+    classes = det_model.names
+    name = classes.values()
+    detection_result = {class_id: 0 for class_id in name}
+    list = []  
+
     try:
+        # if button == true:
+
         while True:
-            if image_storage[0] is not None:
-                stitched_image = image_storage[0].copy()
-                results = det_model.track(stitched_image, persist=True)
+                if image_storage[0] is not None:
+                    stitched_image = image_storage[0].copy()
+                    results = det_model.track(stitched_image, persist=True)
 
-                for result in results:
-                    plot_image = result.plot()
-                    if plot_image is not None:
-                        cv2.imshow('Object Detection', plot_image)
+                    # Initialize class_counts for the current frame
+                    # class_counts = {name: 0 for name in classes}
+                    
+                    for result in results:
+                        plot_image = result.plot()
+                        if plot_image is not None:
+                            cv2.imshow('Object Detection', plot_image)
 
-                frame_count += 1
-                total_time = time.time() - prev_time
+                        # Increment class counts
+                        for box in result.boxes:
+                            class_id = int(box.cls)
+                            obj_id = box.id
+                            pred_class = classes[class_id]
+                            if obj_id not in list:
+                                list.append(obj_id)
+                                if pred_class in detection_result.keys():
+                                    detection_result[pred_class] += 1
 
-                if total_time >= 1.0:
-                    fps = frame_count / total_time
-                    print(f"FPS: {fps:.2f}")
-                    frame_count = 0
-                    prev_time = time.time()
-                
-                    # Encode frame as JPEG
-                    _, buffer = cv2.imencode('.jpg', plot_image)
-                    frame_data = base64.b64encode(buffer).decode('utf-8')
+                    print(detection_result)
+                    frame_count += 1
+                    total_time = time.time() - prev_time
 
-                    # Send frame over WebSocket
-                    socketio.emit('video_frame',{'frame':frame_data})
+                    if total_time >= 1.0:
+                        fps = frame_count / total_time
+                        print(f"FPS: {fps:.2f}")
+                        frame_count = 0
+                        prev_time = time.time()
+                    
+                        # Encode frame as JPEG
+                        _, buffer = cv2.imencode('.jpg', plot_image)
+                        frame_data = base64.b64encode(buffer).decode('utf-8')
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+                        # Send frame over WebSocket
+                        socketio.emit('video_frame',{'frame':frame_data})
+                        socketio.emit('detection_result',{'result':detection_result})
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
     finally:
-        stop_event.set()
-        camera1.StopGrabbing()
-        camera1.Close()
-        cv2.destroyAllWindows()
-        thread1.join()
-
-
-
-
-
-
-        # # Calculate and display FPS
-        # frame_count += 1
-        # elapsed_time = time.time() - start_time
-        # fps = frame_count / elapsed_time
-        # cv2.putText(img, f"FPS: {fps:.2f}", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        # # Show webcam feed
-        # cv2.imshow('Webcam', img)
-        # # Quit if 'q' is pressed
-        # if cv2.waitKey(1) == ord('q'):
-        #     break
-
-
-        # Control frame rate
-        # await asyncio.sleep(0.04)  # Adjust to match video FPS
-
-
+            stop_event.set()
+            camera1.StopGrabbing()
+            camera1.Close()
+            cv2.destroyAllWindows()
+            thread1.join()
+    
 #app instance
 app=Flask(__name__)
-# socketio = SocketIO(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
-
-# @app.route("/api/start",methods=['GET'])
-# def detect_start():
-#     return jsonify({
-#         'message': "Start"
-#     })
-
-# @app.route("/api/stop",methods=['GET'])
-# def detect_stop():
-#     return jsonify({
-#         'message': "Stop"
-#     })
+# socketio = SocketIO(app)
 
 @socketio.on("connect")
 def connect(*args, **kwargs):
@@ -190,12 +167,6 @@ def disconnect(*args, **kwargs):
     return "Disconnected"
 
 if __name__ == "__main__":
-
-    # app.run(debug=True)
-
-    # start_server = websockets.serve(video_stream, "localhost", 1357)
-    # asyncio.get_event_loop().run_until_complete(start_server)
-    # asyncio.get_event_loop().run_forever()
 
     try:
         # NOTE: Use Thread to solve the issue, there might be a better solution without multiple thread
