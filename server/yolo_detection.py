5689e181eb22327a490fab663050e500a2a80e69
Beh Chun Siang <chunsiang1109@gmail.com>
2024-08-06 16:48:53 +0800

    post method for capturing one image when non streaming mode

--- server/yolo_detection.py
+++ server/yolo_detection.py
@@ -1,11 +1,15 @@
 from ultralytics import YOLO
 from flask import Flask,jsonify
+from flask_cors import CORS
 from flask_socketio import SocketIO
 import cv2
 import math
 import time
 import numpy as np
+
 import threading
+from threading import Lock
+
 from pypylon import pylon
 from ultralytics import YOLO
 from pathlib import Path
@@ -13,31 +17,19 @@ import openvino as ov
 import torch
 import base64
 
+#to control the detection of the camera
+detection_running= False
+last_capture_frame = None
+last_detection_result = None
+mutex_lock = Lock()
+
+# Stop event for stopping the detection loop
+stop_event = threading.Event()
 
 # Start webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
-
-# Model
-model = YOLO("yolov8n.pt")
-
-# Object classes
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"]
-
-# FPS calculation variables
-start_time = time.time()
-frame_count = 0
-
 
 def capture_image_from_basler_camera(camera, converter):
     grabResult = camera.RetrieveResult(2000, pylon.TimeoutHandling_Return)
@@ -50,7 +42,6 @@ def capture_image_from_basler_camera(camera, converter):
     else:
         grabResult.Release()
         return None
-
 
 def resize_with_aspect_ratio(image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
@@ -65,28 +56,28 @@ def resize_with_aspect_ratio(image, width=None, height=None, inter=cv2.INTER_ARE
         dim = (width, int(h * r))
     return cv2.resize(image, dim, interpolation=inter)
 
-
 def capture_and_store(camera, converter, storage, index, stop_event):
     while not stop_event.is_set() and camera.IsGrabbing():
-        img = capture_image_from_basler_camera(camera, converter)
-        storage[0] = img
-
+            img = capture_image_from_basler_camera(camera, converter)
+            storage[0] = img
+    
 def video_stream():
+    print("yolo_stream_detection")
+    global start_time, frame_count, detection_running,stop_event, mutex_lock
+    global last_capture_frame, last_detection_result
+
     # Initialize the Basler camera interface
     tl_factory = pylon.TlFactory.GetInstance()
     devices = tl_factory.EnumerateDevices()
     if not devices:
         print("Error: No Basler camera found.")
         return
-    
-    IMAGE_PATH = Path("./data/1.jpg")
+
     models_dir = Path("./models")
     DET_MODEL_NAME = "best"
     det_model = YOLO(models_dir / f"{DET_MODEL_NAME}.pt")
-  
 
     camera1 = pylon.InstantCamera(tl_factory.CreateDevice(devices[0]))
-
     camera1.Open()
     camera1.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
 
@@ -95,116 +86,114 @@ def video_stream():
     converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
     image_storage = [None]
-    stop_event = threading.Event()
+    stop_event.clear()
 
     thread1 = threading.Thread(target=capture_and_store, args=(camera1, converter, image_storage, 0, stop_event))
-
     thread1.start()
 
     frame_count = 0
     prev_time = time.time()
 
+    # Table for results
+    classes = det_model.names
+    name = classes.values() 
+    detection_result = {class_id: 0 for class_id in name}
+    list = []
+
     try:
         while True:
+            if not detection_running:
+                # time.sleep(1)
+                continue
+
+        # try:
             if image_storage[0] is not None:
                 stitched_image = image_storage[0].copy()
                 results = det_model.track(stitched_image, persist=True)
 
                 for result in results:
                     plot_image = result.plot()
-                    if plot_image is not None:
-                        cv2.imshow('Object Detection', plot_image)
+                    
+                    # Increment class counts
+                    for box in result.boxes:
+                        class_id = int(box.cls)
+                        obj_id = box.id
+                        pred_class = classes[class_id]
+                        
+                        if obj_id not in list:
+                            list.append(obj_id)
 
+                            if pred_class in detection_result.keys():
+                                detection_result[pred_class] += 1
+                                
                 frame_count += 1
                 total_time = time.time() - prev_time
+                frame_count = 0
+                prev_time = time.time()
+            
+                # Encode frame as JPEG
+                _, buffer = cv2.imencode('.jpg', plot_image)
+                frame_data = base64.b64encode(buffer).decode('utf-8')
 
-                if total_time >= 1.0:
-                    fps = frame_count / total_time
-                    print(f"FPS: {fps:.2f}")
-                    frame_count = 0
-                    prev_time = time.time()
+                # Send frame over WebSocket
+                socketio.emit('video_frame',{'frame':frame_data,'result':detection_result})
+                last_capture_frame = frame_data
+                last_detection_result = detection_result
                 
-                    # Encode frame as JPEG
-                    _, buffer = cv2.imencode('.jpg', plot_image)
-                    frame_data = base64.b64encode(buffer).decode('utf-8')
+                if stop_event.is_set():
+                    socketio.emit('clear_display')
+                    break
 
-                    # Send frame over WebSocket
-                    socketio.emit('video_frame',{'frame':frame_data})
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
     finally:
         stop_event.set()
         camera1.StopGrabbing()
         camera1.Close()
-        cv2.destroyAllWindows()
         thread1.join()
 
-
-
-
-
-
-        # # Calculate and display FPS
-        # frame_count += 1
-        # elapsed_time = time.time() - start_time
-        # fps = frame_count / elapsed_time
-        # cv2.putText(img, f"FPS: {fps:.2f}", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        # # Show webcam feed
-        # cv2.imshow('Webcam', img)
-        # # Quit if 'q' is pressed
-        # if cv2.waitKey(1) == ord('q'):
-        #     break
-
-
-        # Control frame rate
-        # await asyncio.sleep(0.04)  # Adjust to match video FPS
-
-
+    
 #app instance
 app=Flask(__name__)
-# socketio = SocketIO(app)
+CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# @app.route("/api/start",methods=['GET'])
-# def detect_start():
-#     return jsonify({
-#         'message': "Start"
-#     })
+@app.post("/get_last_capture_image")
+def send_last_capture_image():
+    global mutex_lock, last_capture_frame, last_detection_result
+    with mutex_lock:
+        try:
+            response = {
+                "latest_image": last_capture_frame,
+                "detection_result": last_detection_result
+            }
+            return jsonify(response), 200
 
-# @app.route("/api/stop",methods=['GET'])
-# def detect_stop():
-#     return jsonify({
-#         'message': "Stop"
-#     })
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+        
+@socketio.on('connect')
+def handle_connect():
+    global detection_running, mutex_lock
+    mutex_lock.acquire()
+    detection_running = True
+    mutex_lock.release()
+    print("Client connected")
 
-@socketio.on("connect")
-def connect(*args, **kwargs):
-    # reject all connections to test
-    return "Connected"
-
-@socketio.on("disconnect")
-def disconnect(*args, **kwargs):
-    # reject all connections to test
-    return "Disconnected"
+@socketio.on('disconnect')
+def handle_disconnect():
+    global detection_running, stop_event, mutex_lock
+    mutex_lock.acquire()
+    detection_running = False
+    stop_event.set()
+    socketio.emit('clear_display')
+    mutex_lock.release()
+    print("Client disconnected")
 
 if __name__ == "__main__":
 
-    # app.run(debug=True)
-
-    # start_server = websockets.serve(video_stream, "localhost", 1357)
-    # asyncio.get_event_loop().run_until_complete(start_server)
-    # asyncio.get_event_loop().run_forever()
-
     try:
-        # NOTE: Use Thread to solve the issue, there might be a better solution without multiple thread
         threading.Thread(target=video_stream, daemon=True).start()
         socketio.run(app, host='127.0.0.1', port=5000)    
     except KeyboardInterrupt:
         pass
 
-    # Release resources
     cap.release()
-    # cv2.destroyAllWindows()
-
