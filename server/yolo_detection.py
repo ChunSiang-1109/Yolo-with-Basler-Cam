33d27c3ab6065a00c8eaa2d91b815d398b571c7b
Beh Chun Siang <chunsiang1109@gmail.com>
2024-08-20 13:21:17 +0800

    add the save image, video and report when the save_Report api is triggered

--- server/yolo_detection.py
+++ server/yolo_detection.py
@@ -1,27 +1,42 @@
 from ultralytics import YOLO
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 from flask_cors import CORS
 from flask_socketio import SocketIO
+from pypylon import pylon
+from ultralytics import YOLO
+from pathlib import Path
+
+from threading import Lock
+import threading
+
+import os
 import cv2
 import math
 import time
 import numpy as np
-
-import threading
-from threading import Lock
-
-from pypylon import pylon
-from ultralytics import YOLO
-from pathlib import Path
 import openvino as ov
 import torch
 import base64
+import datetime
+import json
+import warnings
+
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 #to control the detection of the camera
 detection_running= False
 last_capture_frame = None
 last_detection_result = None
+last_class_percentages = None
+recording=False
+video_writer  = None
+video_file_path = None
+frame_width, frame_height = 640, 480
+captured_images=[]
 mutex_lock = Lock()
+
+#initialize camera for stopgrabbing to restart the program
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
 
 # Stop event for stopping the detection loop
 stop_event = threading.Event()
@@ -60,11 +75,119 @@ def capture_and_store(camera, converter, storage, index, stop_event):
     while not stop_event.is_set() and camera.IsGrabbing():
             img = capture_image_from_basler_camera(camera, converter)
             storage[0] = img
+
+def calculate_class_percentages(detection_result):
+    total_detections = sum(detection_result.values())
+    if total_detections == 0:
+        return {class_name: '0.00' for class_name in detection_result.keys()}
+
+    class_percentages = {}
+    for class_name, count in detection_result.items():
+        percentage = (count / total_detections) * 100
+        class_percentages[class_name] = f"{percentage:.2f}%"
+
+    return class_percentages
+
+def generate_and_save_report():
+    global last_detection_result, last_class_percentages
     
+    if not last_detection_result or not last_class_percentages:
+        return {"error": "No data available to generate the report."}
+
+    report_data = {
+        "detectionResult": last_detection_result,
+        "percentage": last_class_percentages
+    }
+    
+    # Generate the timestamp for the filename
+    malaysia_time = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
+    timestamp = malaysia_time.strftime("%Y%m%d_%H%M")
+
+    # Set the filepath to Desktop/Kahang
+    report_path = os.path.join(os.path.expanduser("~"), "Desktop", "Kahang","Report")
+    # Ensure the directory exists
+    os.makedirs(report_path, exist_ok=True)
+
+    # Set the filename
+    filename = f"Report_{timestamp}.json"
+    filepath = Path(report_path) / filename
+
+    # Save report data as JSON
+    with open(filepath, 'w') as f:
+        json.dump(report_data, f, indent=2)
+
+    return {"message": f"Report saved successfully at {filepath}"}
+    
+def save_all_captured_images():
+    if not captured_images:
+        return {"error": "No images to save."}
+
+    # Generate the timestamp for the folder name
+    malaysia_time = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
+    timestamp = malaysia_time.strftime("%Y%m%d_%H%M")
+    
+    # Create a directory for saving images
+    images_path = os.path.join(os.path.expanduser("~"), "Desktop", "Kahang", "Images", timestamp)
+    os.makedirs(images_path, exist_ok=True)
+
+    # Save all captured images
+    for i, img in enumerate(captured_images):
+        # Encode image as JPEG
+        _, buffer = cv2.imencode('.jpg', img)
+        filename = f"image_{i+1}.jpg"
+        filepath = os.path.join(images_path, filename)
+        with open(filepath, 'wb') as f:
+            f.write(buffer)
+
+    # Clear the list after saving
+    captured_images.clear()
+    
+    return {"message": f"All images saved successfully in {images_path}"}
+
+def save_video(frame_width, frame_height):
+    # Generate the timestamp for the filename
+    malaysia_time = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
+    timestamp = malaysia_time.strftime("%Y%m%d_%H%M")
+
+    # Set the filepath to save the video
+    video_dir = os.path.join(os.path.expanduser("~"), "Desktop", "Kahang", "Video")
+    os.makedirs(video_dir, exist_ok=True)
+    video_file_path = os.path.join(video_dir, f"video_{timestamp}.mp4")
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'H264')
+    video_writer = cv2.VideoWriter(video_file_path, fourcc, 20.0, (frame_width, frame_height))
+    
+    return video_writer, video_file_path
+
+def start_recording(frame_width, frame_height):
+    global recording, video_writer, video_file_path
+    
+    if not recording:
+        print("Recording is already in progress.")
+        return
+    
+    video_writer, video_file_path = save_video(frame_width, frame_height)
+    recording = True
+    print(f"Recording started: {video_file_path}")
+
+def stop_recording():
+    global recording, video_writer,video_file_path
+
+    if recording:
+        print("No recording in progress.")
+        return
+
+    if video_writer is not None:
+        video_writer.release()
+        video_writer = None
+        recording = False
+        print(f"Recording stopped: {video_file_path}")
+
 def video_stream():
     print("yolo_stream_detection")
-    global start_time, frame_count, detection_running,stop_event, mutex_lock
-    global last_capture_frame, last_detection_result
+    global start_time, frame_count, detection_running,stop_event, mutex_lock,video_writer
+    global last_capture_frame, last_detection_result,last_class_percentages
 
     # Initialize the Basler camera interface
     tl_factory = pylon.TlFactory.GetInstance()
@@ -75,6 +198,11 @@ def video_stream():
 
     models_dir = Path("./models")
     DET_MODEL_NAME = "best"
+
+    # Specify the device as 'cuda' if available
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Using device: {device}")
+
     det_model = YOLO(models_dir / f"{DET_MODEL_NAME}.pt")
 
     camera1 = pylon.InstantCamera(tl_factory.CreateDevice(devices[0]))
@@ -103,10 +231,8 @@ def video_stream():
     try:
         while True:
             if not detection_running:
-                # time.sleep(1)
                 continue
 
-        # try:
             if image_storage[0] is not None:
                 stitched_image = image_storage[0].copy()
                 results = det_model.track(stitched_image, persist=True)
@@ -125,32 +251,31 @@ def video_stream():
 
                             if pred_class in detection_result.keys():
                                 detection_result[pred_class] += 1
-                                
-                frame_count += 1
-                total_time = time.time() - prev_time
-                frame_count = 0
-                prev_time = time.time()
-            
+
+                # Calculate class percentages
+                eachclass_percentages  = calculate_class_percentages(detection_result)
+
                 # Encode frame as JPEG
                 _, buffer = cv2.imencode('.jpg', plot_image)
                 frame_data = base64.b64encode(buffer).decode('utf-8')
 
                 # Send frame over WebSocket
-                socketio.emit('video_frame',{'frame':frame_data,'result':detection_result})
+                socketio.emit('video_frame',{'frame':frame_data,'result':detection_result,'percentage':eachclass_percentages})
                 last_capture_frame = frame_data
                 last_detection_result = detection_result
+                last_class_percentages = eachclass_percentages
                 
-                if stop_event.is_set():
-                    socketio.emit('clear_display')
-                    break
+                # Write frame to video file if recording
+                if video_writer is not None:
+                    video_writer.write(plot_image)
 
     finally:
         stop_event.set()
         camera1.StopGrabbing()
         camera1.Close()
         thread1.join()
+        stop_recording()
 
-    
 #app instance
 app=Flask(__name__)
 CORS(app)
@@ -158,18 +283,62 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.post("/get_last_capture_image")
 def send_last_capture_image():
-    global mutex_lock, last_capture_frame, last_detection_result
+    global mutex_lock, last_capture_frame, last_detection_result,last_class_percentages
     with mutex_lock:
         try:
+            if last_capture_frame is not None:
+                # Decode base64 to image
+                img_data = base64.b64decode(last_capture_frame)
+                np_arr = np.frombuffer(img_data, np.uint8)
+                img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+                
+                captured_images.append(img)                
+
             response = {
                 "latest_image": last_capture_frame,
-                "detection_result": last_detection_result
+                "detection_result": last_detection_result,
+                "percentage": last_class_percentages
             }
             return jsonify(response), 200
 
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-        
+
+@app.route('/recordstream', methods=['POST'])
+def record_stream():
+    global recording, video_writer, video_file_path
+    
+    action = request.json.get('action')
+
+
+    if action == 'start':
+        if not recording:
+            start_recording(frame_width, frame_height)
+            return jsonify({"message": "Recording started."}), 200
+        else:
+            return jsonify({"error": "Recording already in progress."}), 400
+
+    elif action == 'stop':
+        if recording:
+            stop_recording()
+            return jsonify({"message": "Recording stopped."}), 200
+        else:
+            return jsonify({"error": "No recording in progress."}), 400
+
+    else:
+        return jsonify({"error": "Invalid action."}), 400
+
+@app.route("/save_report", methods=["GET"])
+def save_report():
+    try:
+        save_video(frame_width, frame_height)
+        report_result = generate_and_save_report()
+        image_result = save_all_captured_images()
+        return jsonify({"report": report_result, "images": image_result}), 200 if "message" in report_result and "message" in image_result  else 400
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 @socketio.on('connect')
 def handle_connect():
     global detection_running, mutex_lock
@@ -184,7 +353,7 @@ def handle_disconnect():
     mutex_lock.acquire()
     detection_running = False
     stop_event.set()
-    socketio.emit('clear_display')
+    camera.StopGrabbing()
     mutex_lock.release()
     print("Client disconnected")
 
--- yolo_ui/src/App.tsx
+++ yolo_ui/src/App.tsx
@@ -1,25 +1,212 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import { styled } from '@mui/material/styles';
+import { io, Socket } from 'socket.io-client';
+import axios, { AxiosError } from 'axios';
+
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
+import WebsocketDisplay from './WebsocketDisplay';
+import BasicButtonGroup from './BasicButtonGroup';
+import ShowCaptureImageDisplay from './ShowCaptureImageDisplay';
+import DetectionResultTable from './DetectionResultTable';
+
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  width: '200px',
+  height: '200px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
+  //to start or stop 
+  const [socket, setSocketInstance] = useState<Socket | null>(null);
+  //button setting
+  const [startStatus, setStartStatus] = useState(false);
+
+  //output video stream
+  const videoRef = useRef<HTMLImageElement>(null);
+  //output capturing one
+  const latestCaptureRef = useRef<HTMLImageElement>(null);
+
+  const [capturedImage, setCapturedImage] = useState(false);
+
+  //output table for streaming stream
+  const [detectionResult, setDetectionResult] = useState<any>(null);
+  const [percentage, setPercentage] = useState<any>(null);
+
+  //output table for non streaming mode
+  const [detectionResultsList, setDetectionResultsList] = useState<any[]>([]);
+
+  //button run
+  const [buttonHide, setButtonHide] = useState<boolean>(false);
+
+  useEffect(() => {
+    return () => {
+      if (socket) {
+        socket.disconnect();
+      }
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    if (startStatus) {
+      connectSocket();
+    }
+    else {
+      disconnectSocket();
+    }
+  }, [startStatus]);
+
+  useEffect(() => {
+    if (capturedImage) {
+      console.log("Capture Image!!!");
+      fetchImage();
+      setCapturedImage(false);
+    }
+  }, [capturedImage]);
+  
+  useEffect(() => {
+    console.log("Updated percentage:", percentage);
+    setPercentage(percentage);
+  }, [percentage]);
+  
+  const connectSocket = () => {
+    const newSocket = io('http://localhost:5000');
+
+    newSocket.on('connect', () => {
+      console.log("Frontend SocketIO connected!");
+    });
+
+    newSocket.on('disconnect', () => {
+      console.log("Frontend SocketIO disconnected!");
+    });
+
+    newSocket.on('video_frame', (data) => {
+      if (videoRef.current) {
+        // console.log("!!!!",data);
+        videoRef.current.src = `data:image/jpeg;base64,${data.frame}`;
+      }
+      if(!buttonHide && videoRef.current){ 
+        //streaming mode for detection result table
+        setDetectionResult(data.result);
+        startRecording();
+        setPercentage(data.percentage || {}); // Ensure percentage is included
+      }
+      if(buttonHide && videoRef.current)
+      {
+        stopRecording();
+      }
+    });
+
+    setSocketInstance(newSocket);
+  };
+
+  const startRecording = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/recordstream', {action: 'start'});
+      console.log('Recording started:', response.data);
+    } catch (error) {
+        console.error('Error starting recording:', error);
+        alert(`Error starting recording: ${error}`);
+    }
+  };
+  
+  const stopRecording = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/recordstream', { action: 'stop' });
+      console.log('Recording stopped:', response.data);
+    } catch (error) {
+        console.error('Error stopping recording:', error);
+    }
+  };
+  
+
+  const fetchImage = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/get_last_capture_image');
+      const { latest_image, detection_result ,percentage} = response.data;
+
+      // Assuming latest_image is a base64 encoded string of the image
+      if (latestCaptureRef.current) {
+        latestCaptureRef.current.src = `data:image1/jpeg;base64,${latest_image}`;
+      }
+      console.log("Detection Result:", detection_result);
+      console.log("Class Percentages:", percentage);
+      
+      setDetectionResult(detection_result);
+      setPercentage(percentage);
+      if(capturedImage && buttonHide){ 
+        //non streaming mode for the cumulative detection result table
+        setDetectionResultsList(prevResults => [...prevResults, detection_result]);
+      }
+    } catch (error) {
+      console.error("Error fetching the image:", error);
+    }
+  };
+
+  const disconnectSocket = () => {
+    if (socket) {
+      socket.disconnect();
+      setSocketInstance(null);
+    }
+  };
+
+  const setReset = () => {
+    if (videoRef.current) {
+      videoRef.current.src = '';
+    }
+    if (latestCaptureRef.current) {
+      latestCaptureRef.current.src = '';
+      }
+    setDetectionResult({});
+    setDetectionResultsList([]);
+  };
+
+  const setReport = () => {
+    fetch('http://127.0.0.1:5000/save_report', {
+      method: 'GET',
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.message) {
+        console.log(data.message);  // Successfully saved
+      } else {
+        console.error(data.error);  // Error message
+      }
+    })
+    .catch(error => {
+      console.error('Error triggering report save:', error);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Grid container spacing={5} justifyContent="center" alignItems="center">
+        <Grid item xs={4}>
+          <WebsocketDisplay showStreamingImage={buttonHide} image={videoRef} />
+        </Grid>
+        <Grid item xs={4}>
+          <ShowCaptureImageDisplay image1={latestCaptureRef} />
+        </Grid>
+        <Grid item xs={4}>
+        <DetectionResultTable detectionResult1={detectionResult} percentage1={percentage} />
+        </Grid>
+      </Grid>
+      <BasicButtonGroup 
+        setButtonStatus1={setStartStatus}
+        StreamingEnabled={buttonHide}
+        setButtonHide1={setButtonHide}
+        setButtonCapOne={setCapturedImage}
+        setReset={setReset}
+        setSave={setReport}
+         />
+    </>
   );
 }
 
